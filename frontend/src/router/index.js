6c340758f0de9942f694f59f9af6e98d707f1be7
isaac <isaac.ingles@gmail.com>
2020-04-18 18:06:25 -0600

    added some logic to automatically delete token if it is expired

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-inner-declarations */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Landing from '../views/Landing'
@@ -119,6 +120,32 @@ router.beforeEach((to, from, next) => {
         params: { nextUrl: to.fullPath }
       })
     } else {
+      const token = localStorage.getItem('token')
+      const now = Date.now()
+
+      function parseJwt (token) {
+        var base64Url = token.split('.')[1]
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+        }).join(''))
+
+        return JSON.parse(jsonPayload)
+      };
+
+      if (now.toString().slice(0, -3) > parseJwt(token).exp) {
+        localStorage.removeItem('token')
+        localStorage.removeItem('userId')
+        localStorage.removeItem('refreshToken')
+        next({
+          path: '/login',
+          params: { nextUrl: to.fullPath }
+        })
+      }
+
+      // console.log(parseJwt(token).exp)
+      // console.log(now.toString().slice(0, -3))
+
       store.dispatch('setAuth', {
         auth: true,
         id: localStorage.getItem('userId'),
